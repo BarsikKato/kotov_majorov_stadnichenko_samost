7bc4b28e12ebc2090f4f1e22b1ef3ef66f518b0b
Priquo <limela.arolf@gmail.com>
2021-11-28 14:59:19 +0300

    Сделала окно с авторизацией и регистрацией, админ = админ/админ

---
+++ app/src/main/java/com/example/kotov_majorov_stadnichenko_samost/Auth.java
@@ -0,0 +1,105 @@
+package com.example.kotov_majorov_stadnichenko_samost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Auth extends AppCompatActivity implements View.OnClickListener {
+
+    TextView txtUsername, txtPassword;
+    Button buttLogin, buttSignIn;
+
+    DBHelper dbHelper;
+    SQLiteDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+
+        buttLogin = findViewById(R.id.buttLogin);
+        buttLogin.setOnClickListener(this);
+        buttSignIn = findViewById(R.id.buttSignIn);
+        buttSignIn.setOnClickListener(this);
+
+        dbHelper = new DBHelper(this);
+        database = dbHelper.getWritableDatabase();
+
+        //Добавление админа в базу для перехода к управлению магазином
+        ContentValues values = new ContentValues();
+        values.put(dbHelper.KEY_USERNAME, "admin");
+        values.put(dbHelper.KEY_PASSWORD, "admin");
+        database.insert(dbHelper.TABLE_USERS, null, values);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.buttLogin:
+                Cursor logCursor = database.query(dbHelper.TABLE_USERS, null, null, null, null, null, null);
+
+                boolean logged = false;
+                if(logCursor.moveToFirst())
+                {
+                    int IndexUsername = logCursor.getColumnIndex(dbHelper.KEY_USERNAME);
+                    int IndexPassword = logCursor.getColumnIndex(dbHelper.KEY_PASSWORD);
+                    do{
+                        if(txtUsername.getText().toString().equals("admin") && txtPassword.getText().toString().equals("admin"))
+                        {
+                            startActivity(new Intent(this, MainActivity.class));
+                            logged = true;
+                            break;
+                        }
+                        if(txtUsername.getText().toString().equals(logCursor.getString(IndexUsername)) && txtPassword.getText().toString().equals(logCursor.getString(IndexPassword)))
+                        {
+                            startActivity(new Intent(this, Shop.class));
+                            logged = true;
+                            break;
+                        }
+                    }while(logCursor.moveToNext());
+                }
+                logCursor.close();
+                if (!logged) Toast.makeText(this, "Введенные логин и пароль не были найдены", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.buttSignIn:
+                Cursor signCursor = database.query(dbHelper.TABLE_USERS, null, null, null, null, null, null);
+
+                boolean finded=false;
+                if(signCursor.moveToFirst())
+                {
+                    int IndexUsername = signCursor.getColumnIndex(dbHelper.KEY_USERNAME);
+                    do{
+                        if(txtUsername.getText().toString().equals(signCursor.getString(IndexUsername)))
+                        {
+                            Toast.makeText(this, "Такой логин уже существует", Toast.LENGTH_LONG).show();
+                            finded = true;
+                            break;
+                        }
+                    }while(signCursor.moveToNext());
+                }
+                if(!finded)
+                {
+                    ContentValues values = new ContentValues();
+                    values.put(dbHelper.KEY_USERNAME, txtUsername.getText().toString());
+                    values.put(dbHelper.KEY_PASSWORD, txtPassword.getText().toString());
+
+                    database.insert(dbHelper.TABLE_USERS, null, values);
+                    Toast.makeText(this, "Вы успешно зарегистрировались!", Toast.LENGTH_LONG).show();
+                }
+                signCursor.close();
+                break;
+        }
+    }
+}
--- app/src/main/java/com/example/kotov_majorov_stadnichenko_samost/DBHelper.java
+++ app/src/main/java/com/example/kotov_majorov_stadnichenko_samost/DBHelper.java
@@ -7,11 +7,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "contactDb";
-    public static final String TABLE_CONTACTS = "contacts";
 
+    public static final String TABLE_CONTACTS = "contacts";
     public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
     public static final String KEY_PRICE = "price";
+
+    public static final String TABLE_USERS = "useres";
+    public static final String KEY_USERID = "_id";
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_PASSWORD = "price";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -21,12 +26,14 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
                 + " integer primary key," + KEY_NAME + " text," + KEY_PRICE + " text" + ")");
-
+        db.execSQL("create table " + TABLE_USERS + "(" + KEY_USERID
+                + " integer primary key," + KEY_USERNAME + " text," + KEY_PASSWORD + " text" + ")");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists " + TABLE_CONTACTS);
+        db.execSQL("drop table if exists " + TABLE_USERS);
 
         onCreate(db);
 
