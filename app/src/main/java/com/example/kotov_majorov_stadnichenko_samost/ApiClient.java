1a2a2f07a6b51f1253a2d16975d0252c697d67c9
Priquo <limela.arolf@gmail.com>
2021-12-17 18:19:41 +0300

    тут работает апиклиент (ура)

---
+++ app/src/main/java/com/example/kotov_majorov_stadnichenko_samost/ApiClient.java
@@ -0,0 +1,26 @@
+package com.example.kotov_majorov_stadnichenko_samost;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    public static Retrofit getRetrofit()
+    {
+
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+        OkHttpClient okHttpClient = new OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("http://api.larntech.net/")
+                .client(okHttpClient)
+                .build();
+
+        return retrofit;
+    }
+}
